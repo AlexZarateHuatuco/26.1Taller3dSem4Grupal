f229f54bfdc0fec924a3fbfc1f6f58f893b772bd
Alex Zarate <azarate45@gmail.com>
2026-04-16 18:17:52 -0500

    Enemy Class Creation

---
+++ Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Taller3dSemana4Grupal26._1
+{
+    internal class Enemy
+    {
+        public virtual float GetLife()
+        {
+            return ActualLife;
+        }
+        public virtual float Hurt()
+        {
+            return ActualLife-Damage;
+        }
+        public virtual int GetDamage()
+        {
+            return Damage;
+        }
+        public virtual bool IsDead()
+        {
+            return isDead;
+        }
+    }
+}
