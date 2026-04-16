ae3812daf3962a1f8a836d3c46f006b2d862a0c9
Atzel7 <axelrojasb@hotmail.com>
2026-04-16 18:41:26 -0500

    EnemyMelee

---
+++ EnemyMelee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Taller3dSemana4Grupal26._1
+{
+    internal class EnemyMelee : Enemy
+    {
+        public EnemyMelee(float life, int damage) : base(life, damage)
+        {
+        }
+
+        
+        public void MeleeAttack()
+        {
+            Console.WriteLine("Ataque cuerpo a cuerpo de " + Damage);
+        }
+    }
+}
