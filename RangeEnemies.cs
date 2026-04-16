4ba6a817a7b47e9d9350e871a1213ea42a365d3a
Alex Zarate <azarate45@gmail.com>
2026-04-16 18:10:47 -0500

    Class Creation

---
+++ RangeEnemies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3dSemana4Grupal26._1
+{
+    internal class RangeEnemies
+    {
+        int RangeEnemy;
+    }
+}
