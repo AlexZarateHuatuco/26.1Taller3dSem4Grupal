cda835cda10bf18d8343f6f6fde4e769ef660aff
D. Lezama <diavalez@gmail.com>
2026-04-16 19:29:38 -0500

    Juego y desarrollo

---
+++ Juego.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3dSemana4Grupal26._1
+{
+    internal class Juego
+    {
+        public Juego()
+        {
+
+        }
+
+        public bool Combat(ClaseJugador j, Enemy e)
+        {
+            //Jugador ClaseJugador = new ClaseJugador(int vida, int dmg);
+            while (j.vida > 0 && e.ActualLife > 0)
+            {
+                //ataque jugador
+                Console.WriteLine("Jugador ataca");
+                e.ActualLife -= j.DanoOcasionado;
+                Console.WriteLine($"HP jugador: {j.vida}\nHP enemigo: {e.ActualLife}");
+                Console.ReadLine();
+                //ataque enemigo
+                Console.WriteLine("Enemigo ataca");
+                j.vida -= e.Damage;
+                Console.WriteLine($"HP jugador: {j.vida}\nHP enemigo: {e.ActualLife}");
+            }
+            //victory check
+            if(e.isDead)
+            {
+                return true;
+                Console.WriteLine("¡VICTORIA!\nSiguiente ronda");
+                Console.ReadLine();
+            }
+            if (j.vida <= 0)
+            {
+                return false;
+                Console.WriteLine("¡DEROTA!\nFin del juego");
+                Console.ReadLine();
+            }
+            
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace Taller3dSemana4Grupal26._1
     {
         static void Main(string[] args)
         {
+            Juego game = new Juego();
+            List<Enemy> enemigos = new List<Enemy>();
+            //crear jugador
+            Console.WriteLine("Ingresa la cantidad de vida del jugador:");
+            int hp = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el daño que ocasiona el jugador:");
+            int dmg = int.Parse(Console.ReadLine());
+            //verificar stats
+            if (hp > 100 || dmg > 100)
+            {
+                Console.WriteLine("No se puede crear al jugador (stats mayores a 100).");
+            }
+            else
+            {
+                ClaseJugador jugador = new ClaseJugador(hp, dmg);
+            }
+            //ejecutar combate (1 enemigo)
+            bool isAlive = true;
+            for (int i = 0; i < enemigos.Count; i++ && isAlive)
+            {
+                //derrota = game over
+                if (Juego.Combat(jugador, enemigos[i]) == false)
+                {
+                    isAlive = false;
+                }
+                //victoria = sgte enemigo
+            }
+            //ya no hay enemigos = victoria
+            Console.WriteLine("Has completado el juego. ¡Felicidades!");
         }
     }
 }
