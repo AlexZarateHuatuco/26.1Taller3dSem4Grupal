ef284ca23e75a9808aa07fd13e0e3e6163b78b5f
Hecateo <juniorcubamontero83@gmail.com>
2026-04-16 19:03:42 -0500

    claseJugador
    clase del jugador

---
+++ ClaseJugador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseJugador
+{
+    internal class ClaseJugador
+    {
+        public int vida;
+        public int DañoOcacionado = 5;
+
+        public ClaseJugador(int VIDA, int DAÑOOCACIONADO)
+        {
+            vida = VIDA;
+            DañoOcacionado = DAÑOOCACIONADO;
+        }
+
+        public int recibirDaño()
+        {
+            return vida-3;
+        }
+        public int dañoOcacionado()
+        {
+            return DañoOcacionado;
+        }
+    }
+}
